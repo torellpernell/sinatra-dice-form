0b8a7f8d141b39b1db5993245e785a278e848aef
Torell T. Pernell <torellpernell@yahoo.com>
2025-02-25 05:10:35 +0000

    first commit

--- app.rb
+++ app.rb
@@ -2,8 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
+  erb(:home)
+end
+
+get("/process_roll/:dice/:sides") do
+  @dice = params.fetch("dice")
+  @sides = params.fetch("sides")
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
+
   "
+
+  erb(:process_roll)
 end
